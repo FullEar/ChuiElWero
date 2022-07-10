f14a61bcd77e6f979aaf7b883575263c7f86bb42
Elliot QR <elliotqr18@gmail.com>
2022-07-10 01:54:07 -0500

    Fall Detector ind the main scene

--- ChuiElWero/Assets/Scripts/DamageObject.cs
+++ ChuiElWero/Assets/Scripts/DamageObject.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class DamageObject : MonoBehaviour
 {
+    private Vector3 respawnPoint;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.transform.CompareTag("Player"))
         {
             Debug.Log("Player Damage");
             Destroy(collision.gameObject);
+          
         }
+
+
     }
 }
